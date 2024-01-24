49102fb3364b7690ac42a318c9904766ead8feed
RKoopal <63960570+RKoopal@users.noreply.github.com>
2024-01-24 18:44:54 +0100

    sample random date

--- src/GetNegatives.py
+++ src/GetNegatives.py
@@ -1,5 +1,6 @@
-import pandas as pd 
 import random
+import datetime
+import pandas as pd 
 
 RF_GRID_COLUMNS = [
     "grid_id",
@@ -26,16 +27,23 @@ RF_GRID_COLUMNS = [
     'Onbekend'
 ]
 
+RANDOM_START_DATE = datetime.datetime(2022, 1, 1)
+RANDOM_END_DATE = datetime.datetime(2023, 12, 31)
+
 class NegativeSampler():
     def __init__(
         self,
         has_column,
         has_tree,
-        window = 5
+        window = 5,
+        random_dates = False,       # to select random dates
+        random_grid = False,        # if you do not want to specify whether a grid has trees
     ):
         self.has_column = has_column
         self.window = window
         self.has_tree = has_tree
+        self.random_dates = random_dates
+        self.random_grid = random_grid
 
     def verify_sample(
         self,
@@ -49,14 +57,43 @@ class NegativeSampler():
 
         return False if grid_id not in grids else True
 
+    def sample_random_dates(
+        self,
+        num_samples,
+        start_date = RANDOM_START_DATE,
+        end_date = RANDOM_END_DATE
+    ):
+        dates = []
+        hours = []
+
+        for _ in range(num_samples):
+            random_datetime = start_date + datetime.timedelta(
+                days=random.randint(0, (end_date - start_date).days),   # sample from 2022-2023
+                hours=random.randint(6, 22)                             # sample between 0600-2200
+            )
+            dates.append(random_datetime.date())
+            hours.append(random_datetime.hour)
+
+        date_df = pd.DataFrame({'Date': dates, 'Hour': hours})
+        return date_df
+
     def sample_negatives(
         self,
         incidents,
         positives,
         grid
     ):
-        grids_to_sample = list(grid[grid[self.has_column] == self.has_tree].grid_id.values)
-        negatives = positives[['Date', 'Hour']]
+        if self.random_grid:
+            grids_to_sample = list(grid.grid_id.values)
+        else:
+            grids_to_sample = list(grid[grid[self.has_column] == self.has_tree].grid_id.values)
+
+        # if sample random dates
+        if self.random_dates:
+            negatives = self.sample_random_dates(num_samples=len(positives))
+        else:
+            negatives = positives[['Date', 'Hour']]
+            
         negatives[RF_GRID_COLUMNS] = None
         for i, row in negatives.iterrows():
             random_grid = random.sample(grids_to_sample, 1)[0]
